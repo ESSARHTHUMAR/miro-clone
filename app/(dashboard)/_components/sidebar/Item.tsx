baf3702dce6c6874580ee48ce4b111500b8cdff2
ESSARHTHUMAR <essarthumar177@gmail.com>
2024-02-09 12:32:37 +0530

    Feature-Tooltip

--- app/(dashboard)/_components/sidebar/Item.tsx
+++ app/(dashboard)/_components/sidebar/Item.tsx
@@ -3,6 +3,7 @@
 import { cn } from "@/lib/utils";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import Image from "next/image";
+import Hint from "./Hint";
 
 interface ItemProps {
   id: string;
@@ -25,13 +26,16 @@ function Item({ id, name, imageUrl }: ItemProps) {
 
   return (
     <div className="aspect-square relative">
+        <Hint label={name} side="right" align="start" sideOffset={18}>
       <Image
         fill
         src={imageUrl}
         alt={name}
         onClick={onClick}
-        className={cn("rounded-md cursor-pointer opacity-75 hover:opacity-100")}
+        className={cn("rounded-md cursor-pointer opacity-75 hover:opacity-100", isActive && "opacity-100")}
+        
       />
+      </Hint>
     </div>
   );
 }
